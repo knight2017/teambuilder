e5479c89a6badba7b26fe39a801e2ea579193eff
Wei Yi <weiyi2017@gmail.com>
2016-06-02 23:05:12 -0700

    first commit

---
+++ team.rb
@@ -0,0 +1,64 @@
+require "sinatra"
+require "sinatra/reloader"
+enable :sessions
+# this enables us to use PATCH and DELETE requests
+use Rack::MethodOverride
+
+
+get "/" do
+@hash = {}
+
+
+erb :team, layout: :layout2
+end
+
+
+post "/build" do
+session[:team] = params[:names].split(',')
+ @alert = "please make team size smaller" if params[:size].to_i > session[:team].length
+ session[:hash] = {}
+ if @alert == nil
+    if params[:method]== "size"
+      session[:hash] = size(session[:team], params[:size])
+    else
+      session[:hash] = number(session[:team], params[:size])
+    end
+ end
+  erb :team, layout: :layout2
+  redirect to("/")
+end
+
+def size(arr, size)
+  teamsize =  size
+  hash={}
+  i  = 0
+  while arr.length > 0
+    team = "Team #{i+1}"
+    temp = []
+    teamsize.to_i.times do
+    temp << arr.delete_at(rand(arr.length))
+    end
+
+    hash[team] = temp.compact
+    i += 1
+  end
+hash
+end
+
+
+def number(arr, num)
+  teamsize =  (arr.length / num.to_i)
+  hash={}
+  i  = 0
+  while arr.length > 0
+    team = "Team #{i+1}"
+    temp = []
+    teamsize.to_i.times do
+    temp << arr.delete_at(rand(arr.length))
+    end
+
+    hash[team] = temp.compact
+    i += 1
+  end
+  hash
+end
